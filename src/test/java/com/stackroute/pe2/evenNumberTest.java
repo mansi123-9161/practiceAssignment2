9bd94a09de639d072c7ca6d02f215c653ac15523
mansi <mansi.chourasia@cgi.com>
2019-07-16 17:24:07 +0530

    updated testcases for practice assignment 2

---
+++ src/test/java/com/stackroute/pe2/evenNumberTest.java
@@ -0,0 +1,52 @@
+package com.stackroute.pe2;
+
+import static org.junit.Assert.*;
+
+public class evenNumberTest {
+    private EvenNumber evenNumber;
+
+    @Before
+    public void setup()
+    {
+        //arrange
+        System.out.println("Inside Before");
+        this.evenNumber=new EvenNumber();
+
+    }
+    @After
+    public void tearDown()
+    {
+        System.out.println("Inside After");
+        evenNumber=null;
+
+    }
+
+    @Test
+    //this testcase check for given number is EvenNumber
+    public void givenNumberShouldReturnEvenNumber()
+    {
+        //act
+        boolean actualResults=evenNumber.isEven(20);
+        boolean expectedResult=true;
+        //assert
+        assertEquals(expectedResult,actualResults);
+
+    }
+    @Test
+    //this testcase check for given number is not a EvenNumber
+    public void givenNumberShouldNotReturnEvenNumber()
+    {    //act
+        boolean actualResult=evenNumber.isEven(21);
+        boolean expectedResult=true;
+        //assert
+        assertNotEquals(expectedResult,actualResult);
+
+    }
+    @Test
+    //this testcase check for ErrorMessege if given input is wrong
+    public void givenNumberShouldReturnErrorMessage()
+    {
+        String actualResults=evenNumber.isEven(" ");
+        assertEquals("enter correct input",actualResults);
+    }
+}
